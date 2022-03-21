5651d44667d5b9a53ee23c247a10240cb5bb061a
cmahn <cmahn@yonsei.ac.kr>
2022-03-22 00:16:12 +0900

    20220321 Layout 조정 (커스텀 위젯) v003

--- lib/main.dart
+++ lib/main.dart
@@ -5,68 +5,45 @@ void main() {
   runApp(const MyApp());
 }
 
+// 변수로 위젯 대체: 축약 가능하지만, 성능 이슈가 있을 수 있다.
+var a = SizedBox(
+  child: Text('안녕'),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp( // material 테마 가능 위젯
-      home: Scaffold(
-        appBar: AppBar( // 안에 넣을 수 있는 것들
-          // 컬러
-          backgroundColor: Colors.white,
-          // 왼쪽제목
-          title: Text('금호동3가', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w300)),
-
-          // 왼쪽에 넣을 아이콘
-          // leading: Icon(Icons.),
-          // 우측 아이콘
-          actions: [
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.search, color: Colors.black,)
-            ),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.menu, color: Colors.black,)
-            ),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.doorbell, color: Colors.black,)
+    return MaterialApp(
+        // material 테마 가능 위젯
+        home: Scaffold(
+            appBar: AppBar(),
+            body: ListView(  // 컬럼 대시에 스크롤바가 생긴다, 메모리 절약도 가능-스크롤 내 숨겨진 곳은 메모리에서 삭제 가능
+              // controller: , //스크롤 위치 체크도 가능
+              children: [
+                CstWz(),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+              ],
             )
-          ],
-        ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Flexible(
-                child: Image.asset('assets/cannon.PNG'), flex:5
-              ),
-              Expanded(
-                // width: 300, // 퍼센티지로 넓이 설정
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 판매', ),
-                    Text('금호동 3가'),
-                    Text('7000월'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                )
-              )
-            ],
-          ),
         )
-      )
     );
   }
 }
+
+// 커스텀 위젯: 길고 복잡한 레이아웃 축약
+class CstWz extends StatelessWidget {
+  const CstWz({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('Hello'),
+    );
+  }
+}
+
+
